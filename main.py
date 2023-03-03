d43ee0b7c45c6d374872fd5accf30a81e4609f35
sbsx <sandeshshrestha02@gmail.com>
2023-03-02 22:10:48 -0800

    interactive grid

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# https://www.figma.com/file/loC6txzGfA0dgvmFhopgce/InteractivePortfolio-Draft?node-id=0%3A1&t=LxLrMMrx1MXuGyRb-0
